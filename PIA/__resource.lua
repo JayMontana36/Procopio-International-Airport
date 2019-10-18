1f3a68fd2a275b92e872395fb3e594e08bf65c98
Jasen Samuels <jasensamuels@gmail.com>
2019-10-18 11:50:51 -0400

    Upload/Push The FiveM Resource in it's current state, with credits given to original creator of the map.

---
+++ PIA/__resource.lua
@@ -0,0 +1,14 @@
+-- Procopio International Airport (PIA)
+-- Map by mixtro: https://www.gta5-mods.com/maps/procopio-international-airport
+
+-- Standalone Resource for FiveM
+-- Converted by JayMontana36 for FSRP (Flight Simulator Roleplay) - https://sites.google.com/site/flightsimrp
+
+-- Contains a YMAP generated from the XML
+
+description 'Procopio International Airport (PIA) by mixtro & JayMontana36'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
+
---
+++ README.md
@@ -0,0 +1,12 @@
+Installation:
+1. Drag the PIA folder into the resource folder of your FX Server folder.
+2. Edit your server.cfg and add "start PIA" to the resource list.
+
+All credit for the making of the map is to mixtro.
+
+
+Standalone Resource for FiveM
+Converted by JayMontana36 for FSRP (Flight Simulator Roleplay) - https://sites.google.com/site/flightsimrp
+
+
+
